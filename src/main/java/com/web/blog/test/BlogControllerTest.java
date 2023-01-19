d75095cfd4bdbb28f987a36ac90f4ef5dd55a62a
Jo <unknownc4m@gmail.com>
2023-01-19 22:22:29 +0900

    環境設定完了

---
+++ src/main/java/com/web/blog/test/BlogControllerTest.java
@@ -0,0 +1,16 @@
+package com.web.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+	
+	// 스프링이 내가 만든 com.web.blog이하를 스캔해서 메모리힙에 띄우는 것은 아니고
+	// 특정 애노테이션이 붙어 있는 클래스 파일들만 new해서(IoC)해서 스프링 컨테이너에 관리해준다.
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h2>Hello Spring Boot!</h2>";
+	}
+	
+}
